59f278fc487252234ab096ecc601f93e26307e6e
RaccoonCodes <61885457+RaccoonCodes@users.noreply.github.com>
2024-02-28 15:16:42 -0600

    Progress to version 2: Adding navigation. Cart in progress

--- SportsStore/Infrastructure/PageLinkTagHelper.cs
+++ SportsStore/Infrastructure/PageLinkTagHelper.cs
@@ -27,6 +27,11 @@ namespace SportsStore.Infrastructure
         public PagingInfo? PageModel { get; set; }
         public string? PageAction { get; set; }
 
+        [HtmlAttributeName(DictionaryAttributePrefix = "page-url-")]
+        public Dictionary<string, object> PageUrlValues { get; set; }
+            = new Dictionary<string, object>();
+
+
         public bool PageClassesEnabled { get; set; } = false;
         public string PageClass { get; set; } = String.Empty;
         public string PageClassNormal { get; set; } = String.Empty;
@@ -42,8 +47,9 @@ namespace SportsStore.Infrastructure
                 for (int i = 1; i <= PageModel.TotalPages; i++)
                 {
                     TagBuilder tag = new TagBuilder("a");
+                    PageUrlValues["productPage"] = i;
                     tag.Attributes["href"] = urlHelper.Action(PageAction,
-                    new { productPage = i });
+                    PageUrlValues);
 
                     if (PageClassesEnabled)
                     {
--- SportsStore/Program.cs
+++ SportsStore/Program.cs
@@ -8,14 +8,27 @@ builder.Services.AddDbContext<StoreDbContext>(opts =>{
     opts.UseSqlServer(builder.Configuration["ConnectionStrings:SportsStoreConnection"]);
 });
 builder.Services.AddScoped<IStoreRepository,EFStoreRepository>();
+builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
 
 app.UseStaticFiles();
-app.MapControllerRoute("pagination","Products/Page{productPage}",
+
+app.MapControllerRoute("catpage","{category}/Page{productPage:int}",
     new { Controller = "Home", action = "Index" });
+
+app.MapControllerRoute("page","Page{productPage:int}",
+    new {Controller = "Home", action="Index", producyPage = 1 });
+
+app.MapControllerRoute("category", "{category}",
+    new { Controller = "Home", action = "Index", productPage = 1 });
+
+app.MapControllerRoute("pagination","Products/Page{productPage}",
+    new { Controller = "Home", action = "Index", productPage = 1 });
+
 app.MapDefaultControllerRoute();
+app.MapRazorPages();
 
 SeedData.EnsurePopulated(app);
 
